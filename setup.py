fa399f63e52836a8e4d283173e3b9e0569c084c8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-07-14 21:19:52 +0000

    fix namespace
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.hypatia/trunk@43129 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         ],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
-	namespace_packages=['nti'],
+	namespace_packages=['nti','nti.app'],
 	install_requires=[
 		'setuptools',
         'nti.hypatia',
