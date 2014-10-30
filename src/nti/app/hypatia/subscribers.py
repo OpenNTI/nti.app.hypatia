fb5c17d375e7a7fbf85b1c668b643ff32e25145b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-10-30 23:11:25 +0000

    remove unneeded
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.hypatia/trunk@52593 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/hypatia/subscribers.py
+++ src/nti/app/hypatia/subscribers.py
@@ -7,26 +7,3 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-import functools
-import transaction
-
-from zope import component
-
-from pyramid.interfaces import INewRequest
-
-from nti.dataserver.interfaces import IDataserverTransactionRunner
-
-from nti.hypatia import process_queue
-
-@component.adapter(INewRequest)
-def requestIndexation(event):
-	def _process_event():
-		transaction_runner = \
-			component.getUtility(IDataserverTransactionRunner)
-		func = functools.partial(process_queue, limit=-1)
-		transaction_runner(func)
-		return True
-
-	transaction.get().addAfterCommitHook(
-					lambda success: success and _process_event())
